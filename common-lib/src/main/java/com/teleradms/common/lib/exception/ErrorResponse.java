892d16326090a6b58eda5f0409cef0dc672b1349
orhan <orhantrkmn749@gmail.com>
2025-06-07 09:36:31 +0300

    feat(common-lib): add global validation exception handler with standardized error response

---
+++ common-lib/src/main/java/com/teleradms/common/lib/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.teleradms.common.lib.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String message;
+    private List<String> errors;
+    private String path;
+}
---
+++ common-lib/src/main/java/com/teleradms/common/lib/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.teleradms.common.lib.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest request) {
+
+        List<String> errorMessages = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> fieldError.getField() + ": " + fieldError.getDefaultMessage())
+                .toList();
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .errors(errorMessages)
+                .message("VALIDATION FAILED")
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+}
