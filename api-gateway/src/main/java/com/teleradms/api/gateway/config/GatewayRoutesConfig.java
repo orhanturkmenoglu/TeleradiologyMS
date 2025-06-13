fdb0f3af3f1009644f0f5006244506eb0c1e512f
orhan <orhantrkmn749@gmail.com>
2025-06-13 14:35:16 +0300

    api-gateway gereksiz importlar kaldırıldı.

--- api-gateway/src/main/java/com/teleradms/api/gateway/config/GatewayRoutesConfig.java
+++ api-gateway/src/main/java/com/teleradms/api/gateway/config/GatewayRoutesConfig.java
@@ -1,14 +1,9 @@
 package com.teleradms.api.gateway.config;
 
-import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Mono;
-
-import java.util.Optional;
 
 @Configuration
 public class GatewayRoutesConfig {
